eb26492675d59f18fcc5a493a2e218cf0bd0f673
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-04-07 22:52:28 +0200

    Better setup

---
+++ bf1/__init__.py
@@ -0,0 +1,12 @@
+"""
+Battlefield 1 SDK
+"""
+
+__title__ = 'Battlefield 1 SDK'
+__version__ = '0.1'
+__author__ = 'Alessandro De Angelis'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Alessandro De Angelis'
+
+# Version synonym
+VERSION = __version__
--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `__init__.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('bf1')
+
+LONG_DESCRIPTION = open('README.rst').read()
 
 
 setup(
     name="battlefield1-sdk",
-    version="0.1",
-    description="Battlefield 1 SDK python",
-    long_description=open('README.rst').read(),
+    version=version,
     author="Alessandro De Angelis",
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-        'pyOpenSSL'
-    ]
+    author_email="alessandrodea22@gmail.com",
+    description="Battlefield 1 sdk",
+    long_description=LONG_DESCRIPTION,
+    license="MIT",
+    keywords="Battlefield 1 SDK",
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['requests', 'pyOpenSSL'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
 )
