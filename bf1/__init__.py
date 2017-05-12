6141cc13825d0e3b14cb14933285affa8ba9d87e
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-05-12 12:26:02 +0200

    Bumped 1.1

--- bf1/__init__.py
+++ bf1/__init__.py
@@ -3,7 +3,7 @@ Battlefield 1 SDK
 """
 
 __title__ = 'Battlefield 1 SDK'
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'Alessandro De Angelis'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Alessandro De Angelis'
